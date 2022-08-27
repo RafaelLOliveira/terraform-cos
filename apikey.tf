e52d779e0540cd8cc5b084085605ba1f3b61113a
Rafael <rafaeloliveira@ibm.com>
2022-08-26 22:15:48 -0300

    primeiro commit

---
+++ apikey.tf
@@ -0,0 +1,3 @@
+variable "ibmcloud_api_key" {
+  default = "dvfhB4BK3bJN3jgYQft6GW2JwdBuPiPn7q8Cp_Sk3Y88"
+}
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "bucket_name" {
+  default = "bucket-teste"
+}
+
+variable "resource_group_name" {
+  default = "Default"
+}
+
+variable "region_location" {
+  default = "us-east"
+}
+
+variable "storage_class" {
+  default = "standard"
+}
