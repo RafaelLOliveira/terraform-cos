12d3cc80a9e30398b8de0d78f1e10d0d421578cc
Rafael <rafaeloliveira@ibm.com>
2022-08-29 11:03:28 -0300

    alteracoes

--- variables.tf
+++ variables.tf
@@ -3,15 +3,20 @@ variable "bucket_name" {
 }
 
 variable "resource_group_name" {
-  default = "Default"
+  description = "Enter Name of the resource group"
+  default = "RG-Rafael"
 }
 
 variable "region_location" {
+  description = "COS location info"
   default = "us-east"
 }
 
 variable "storage_class" {
+  description = "Storage class to use for the bucket."
   default = "standard"
 }
 
-variable "ibmcloud_api_key" {}
+variable "ibmcloud_api_key" {
+  description = "Enter your API Key"
+}
